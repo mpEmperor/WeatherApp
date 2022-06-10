a62a9cc1f654f07c580b7c5a537ec7b33d324bb9
rpage0630 <rpage0630@gmail.com>
2022-06-10 12:00:34 -0400

    City Screen

--- src/main/java/com/example/fse/Astro.java
+++ src/main/java/com/example/fse/Astro.java
@@ -8,4 +8,51 @@ public class Astro {
     private String moon_phase;
     private String moon_illumination;
 
+    public String getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(String sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public String getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(String sunset) {
+        this.sunset = sunset;
+    }
+
+    public String getMoonrise() {
+        return moonrise;
+    }
+
+    public void setMoonrise(String moonrise) {
+        this.moonrise = moonrise;
+    }
+
+    public String getMoonset() {
+        return moonset;
+    }
+
+    public void setMoonset(String moonset) {
+        this.moonset = moonset;
+    }
+
+    public String getMoon_phase() {
+        return moon_phase;
+    }
+
+    public void setMoon_phase(String moon_phase) {
+        this.moon_phase = moon_phase;
+    }
+
+    public String getMoon_illumination() {
+        return moon_illumination;
+    }
+
+    public void setMoon_illumination(String moon_illumination) {
+        this.moon_illumination = moon_illumination;
+    }
 }
--- src/main/java/com/example/fse/MainController.java
+++ src/main/java/com/example/fse/MainController.java
@@ -35,8 +35,8 @@ public class MainController {
         System.out.println(hour);
         if (weather.getCurrent().getIs_day() == 1) {
             bgImageView.setImage(new Image(getClass().getResourceAsStream("Extremely modern weather (Instagram story) (1).jpg")));
-
         }
+
         timeLabel.setText(time);
         tempLabel.setText(String.valueOf(weather.getCurrent().getTemp_c()).concat("°C"));
         locationLabel.setText(weather.getLocation().getName());
