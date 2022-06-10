35748919fbdd7c28819e6710c69b6335172c19c5
mpEmperor <multaniprabnoor71@gmail.com>
2022-06-10 12:44:00 -0400

    Weather App

--- src/main/java/com/example/fse/MainController.java
+++ src/main/java/com/example/fse/MainController.java
@@ -10,9 +10,10 @@ public class MainController {
     @FXML
     AnchorPane anchorPane;
     @FXML
-    Label tempLabel, timeLabel, locationLabel;
+    Label tempLabel, timeLabel, locationLabel, windLabel, precipitationLabel;
     @FXML
     ImageView imageView, bgImageView;
+
 
     private static Weather weather;
 
@@ -29,16 +30,18 @@ public class MainController {
         if (hour > 12) {
             time = time.replace(String.valueOf(hour), String.valueOf(hour - 12));
             time += " pm";
+        } else if (hour == 12) {
+            time += "pm";
         } else {
             time += " am";
         }
-        System.out.println(hour);
         if (weather.getCurrent().getIs_day() == 1) {
             bgImageView.setImage(new Image(getClass().getResourceAsStream("Extremely modern weather (Instagram story) (1).jpg")));
         } else {
             bgImageView.setImage(new Image(getClass().getResourceAsStream("Extremely modern weather (Instagram story) (2).jpg")));
-
         }
+        windLabel.setText(weather.getCurrent().getWind_dir() + System.lineSeparator() + weather.getCurrent().getWind_kph() + " km/h");
+        precipitationLabel.setText(String.valueOf(weather.getCurrent().getPrecip_mm()) + " mm");
         timeLabel.setText(time);
         tempLabel.setText(String.valueOf(weather.getCurrent().getTemp_c()).concat("°C"));
         locationLabel.setText(weather.getLocation().getName());
