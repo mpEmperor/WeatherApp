74ed83a4f6d729456b15ad13b2f4fa23738f579f
rpage0630 <rpage0630@gmail.com>
2022-06-14 09:14:45 -0400

    City Screen

--- src/main/java/com/example/fse/MainController.java
+++ src/main/java/com/example/fse/MainController.java
@@ -1,7 +1,9 @@
 package com.example.fse;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -10,12 +12,13 @@ public class MainController {
     @FXML
     AnchorPane anchorPane;
     @FXML
-    Label tempLabel, timeLabel, locationLabel, windLabel, precipitationLabel;
+    Label tempLabel, timeLabel, locationLabel,countryLabel, windLabel,conditionLabel,LastUpdatedLabel,feelsLikeLabel;
     @FXML
-    ImageView imageView, bgImageView, windImage, precipitationImage;
+    ImageView imageView, bgImageView, windImage;
 
 
     private static Weather weather;
+
 
     public static Weather getWeather() {
         return weather;
@@ -25,6 +28,8 @@ public class MainController {
         MainController.weather = weather;
     }
     public void initialize() {
+
+
         String time = weather.getLocation().getLocaltime().split(" ")[1];
         int hour = Integer.parseInt(time.split(":")[0]);
         if (hour > 12) {
@@ -35,6 +40,8 @@ public class MainController {
         } else {
             time += " am";
         }
+
+        //Background
         if (weather.getCurrent().getIs_day() == 1) {
             if (weather.getCurrent().getCondition().getText().equals("Sunny")) {
                 bgImageView.setImage(new Image(getClass().getResourceAsStream("SunnyDay.jpg")));
@@ -51,13 +58,20 @@ public class MainController {
             }
             bgImageView.setImage(new Image(getClass().getResourceAsStream("SunnyNight.jpg")));
         }
+
+
         windLabel.setText(weather.getCurrent().getWind_dir() + System.lineSeparator() + weather.getCurrent().getWind_kph() + " km/h");
         windImage.setImage(new Image(getClass().getResourceAsStream("wind.png")));
-        precipitationImage.setImage(new Image("https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather07-512.png"));
-        precipitationLabel.setText(String.valueOf(weather.getCurrent().getPrecip_mm()) + " mm");
+        //precipitationImage.setImage(new Image("https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather07-512.png"));
+        //precipitationLabel.setText(String.valueOf(weather.getCurrent().getPrecip_mm()) + " mm");
         timeLabel.setText(time);
         tempLabel.setText(String.valueOf(weather.getCurrent().getTemp_c()).concat("°C"));
         locationLabel.setText(weather.getLocation().getName());
+        countryLabel.setText(weather.getLocation().getCountry());
+        conditionLabel.setText(weather.getCurrent().getCondition().getText());
+        LastUpdatedLabel.setText("Last Updated: " + weather.getCurrent().getLast_updated());
+        feelsLikeLabel.setText("Feels Like: " + weather.getCurrent().getFeelslike_c() + "°C");
         imageView.setImage(new Image("http://"+weather.getCurrent().getCondition().getIcon().substring(2)));
+
     }
 }
