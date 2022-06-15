d88f871008ba3713e96bb8199684e67e52d59942
mpEmperor <multaniprabnoor71@gmail.com>
2022-06-15 12:17:43 -0400

    Weather App

--- src/main/java/com/example/fse/Forecast.java
+++ src/main/java/com/example/fse/Forecast.java
@@ -5,7 +5,13 @@ import java.util.ArrayList;
 public class Forecast {
     private ArrayList<ForecastDay> forecastday;
 
-
+    public float getAverage() {
+        int sum = 0;
+        for (int i = 0; i < forecastday.size(); i++) {
+            sum += forecastday.get(i).getDay().getAvgtemp_c();
+        }
+        return sum / forecastday.size();
+    }
     public ArrayList<ForecastDay> getForecastday() {
         return forecastday;
     }
