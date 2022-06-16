a19864c622b9e955be5572340c994a73d7bca00e
rpage0630 <rpage0630@gmail.com>
2022-06-16 12:13:54 -0400

    City Screen

--- src/main/java/com/example/fse/MainController.java
+++ src/main/java/com/example/fse/MainController.java
@@ -20,9 +20,9 @@ public class MainController {
     @FXML
     AnchorPane anchorPane;
     @FXML
-    Label tempLabel, timeLabel, locationLabel,countryLabel, windLabel, humidityLabel, pressureLabel,uvLabel, conditionLabel,LastUpdatedLabel,feelsLikeLabel, date, hour1Label, hour2Label, hour3Label, hour4Label, hour5Label, hour6Label, hour7Label, hour8Label, hour9Label, hour10Label, hour11Label, hour12Label, hour13Label, hour14Label, hour15Label, hour16Label, hour17Label, hour18Label, hour19Label, hour20Label, hour21Label, hour22Label, hour23Label, hour24Label,temp1Label, temp2Label, temp3Label, temp4Label, temp5Label, temp6Label, temp7Label, temp8Label, temp9Label, temp10Label, temp11Label, temp12Label, temp13Label, temp14Label, temp15Label, temp16Label, temp17Label, temp18Label, temp19Label, temp20Label, temp21Label, temp22Label, temp23Label, temp24Label;
+    Label tempLabel, timeLabel, locationLabel,countryLabel, windLabel, humidityLabel, pressureLabel,uvLabel, conditionLabel,LastUpdatedLabel,feelsLikeLabel, date, hour1Label, hour2Label, hour3Label, hour4Label, hour5Label, hour6Label, hour7Label, hour8Label, hour9Label, hour10Label, hour11Label, hour12Label, hour13Label, hour14Label, hour15Label, hour16Label, hour17Label, hour18Label, hour19Label, hour20Label, hour21Label, hour22Label, hour23Label, hour24Label,temp1Label, temp2Label, temp3Label, temp4Label, temp5Label, temp6Label, temp7Label, temp8Label, temp9Label, temp10Label, temp11Label, temp12Label, temp13Label, temp14Label, temp15Label, temp16Label, temp17Label, temp18Label, temp19Label, temp20Label, temp21Label, temp22Label, temp23Label, temp24Label, temp1Label1, temp2Label1, temp3Label1, temp4Label1, temp5Label1, temp6Label1, temp7Label1, temp8Label1, temp9Label1, temp10Label1, temp11Label1, temp12Label1, temp13Label1, temp14Label1, temp15Label1, temp16Label1, temp17Label1, temp18Label1, temp19Label1, temp20Label1, temp21Label1, temp22Label1, temp23Label1, temp24Label1, hour1Label1, hour2Label1, hour3Label1, hour4Label1, hour5Label1, hour6Label1, hour7Label1, hour8Label1, hour9Label1, hour10Label1, hour11Label1, hour12Label1, hour13Label1, hour14Label1, hour15Label1, hour16Label1, hour17Label1, hour18Label1, hour19Label1, hour20Label1, hour21Label1, hour22Label1, hour23Label1, hour24Label1;
     @FXML
-    ImageView imageView, bgImageView, windImage, humidityImage1, humidityImage2, pressureImage, uvImage, hour1Image, hour2Image, hour3Image, hour4Image, hour5Image, hour6Image, hour7Image, hour8Image, hour9Image, hour10Image, hour11Image, hour12Image, hour13Image, hour14Image, hour15Image, hour16Image, hour17Image, hour18Image, hour19Image, hour20Image, hour21Image, hour22Image, hour23Image, hour24Image;
+    ImageView imageView, bgImageView, windImage, humidityImage1, humidityImage2, pressureImage, uvImage, hour1Image, hour2Image, hour3Image, hour4Image, hour5Image, hour6Image, hour7Image, hour8Image, hour9Image, hour10Image, hour11Image, hour12Image, hour13Image, hour14Image, hour15Image, hour16Image, hour17Image, hour18Image, hour19Image, hour20Image, hour21Image, hour22Image, hour23Image, hour24Image, hour1Image1, hour2Image1, hour3Image1, hour4Image1, hour5Image1, hour6Image1, hour7Image1, hour8Image1, hour9Image1, hour10Image1, hour11Image1, hour12Image1, hour13Image1, hour14Image1, hour15Image1, hour16Image1, hour17Image1, hour18Image1, hour19Image1, hour20Image1, hour21Image1, hour22Image1, hour23Image1, hour24Image1;
     @FXML
     CheckBox changeBox;
     Stage stage;
@@ -52,7 +52,8 @@ public class MainController {
             time += " am";
         }
 
-        hourly();
+        hourlyNextDay();
+        hourlyCurrentDay();
 
         //Background
         if (weather.getCurrent().getIs_day() == 1) {
@@ -169,7 +170,8 @@ public class MainController {
         stage.show();
     }
 
-    public void hourly(){
+    public void hourlyNextDay(){
+        date.setText("" + weather.getForecast().getForecastday().get(1).getDate());
         hour1Label.setText("12am");
         hour2Label.setText("1am");
         hour3Label.setText("2am");
@@ -244,5 +246,79 @@ public class MainController {
         temp24Label.setText("" + weather.getForecast().getForecastday().get(1).getHour().get(23).getTemp_c() + "°C");
 
     }
+    public void hourlyCurrentDay(){
+        date.setText("" + weather.getForecast().getForecastday().get(1).getDate());
+        hour1Label1.setText("12am");
+        hour2Label1.setText("1am");
+        hour3Label1.setText("2am");
+        hour4Label1.setText("3am");
+        hour5Label1.setText("4am");
+        hour6Label1.setText("5am");
+        hour7Label1.setText("6am");
+        hour8Label1.setText("7am");
+        hour9Label1.setText("8am");
+        hour10Label1.setText("9am");
+        hour11Label1.setText("10am");
+        hour12Label1.setText("11am");
+        hour13Label1.setText("12pm");
+        hour14Label1.setText("1pm");
+        hour15Label1.setText("2pm");
+        hour16Label1.setText("3pm");
+        hour17Label1.setText("4pm");
+        hour18Label1.setText("5pm");
+        hour19Label1.setText("6pm");
+        hour20Label1.setText("7pm");
+        hour21Label1.setText("8pm");
+        hour22Label1.setText("9pm");
+        hour23Label1.setText("10pm");
+        hour24Label1.setText("11pm");
+        hour1Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(0).getCondition().getIcon().substring(2)));
+        hour2Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(1).getCondition().getIcon().substring(2)));
+        hour3Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(2).getCondition().getIcon().substring(2)));
+        hour4Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(3).getCondition().getIcon().substring(2)));
+        hour5Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(4).getCondition().getIcon().substring(2)));
+        hour6Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(5).getCondition().getIcon().substring(2)));
+        hour7Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(6).getCondition().getIcon().substring(2)));
+        hour8Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(7).getCondition().getIcon().substring(2)));
+        hour9Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(8).getCondition().getIcon().substring(2)));
+        hour10Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(9).getCondition().getIcon().substring(2)));
+        hour11Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(10).getCondition().getIcon().substring(2)));
+        hour12Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(11).getCondition().getIcon().substring(2)));
+        hour13Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(12).getCondition().getIcon().substring(2)));
+        hour14Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(13).getCondition().getIcon().substring(2)));
+        hour15Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(14).getCondition().getIcon().substring(2)));
+        hour16Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(15).getCondition().getIcon().substring(2)));
+        hour17Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(16).getCondition().getIcon().substring(2)));
+        hour18Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(17).getCondition().getIcon().substring(2)));
+        hour19Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(18).getCondition().getIcon().substring(2)));
+        hour20Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(19).getCondition().getIcon().substring(2)));
+        hour21Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(20).getCondition().getIcon().substring(2)));
+        hour22Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(21).getCondition().getIcon().substring(2)));
+        hour23Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(22).getCondition().getIcon().substring(2)));
+        hour24Image1.setImage(new Image("http://" + weather.getForecast().getForecastday().get(0).getHour().get(23).getCondition().getIcon().substring(2)));
+        temp1Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(0).getTemp_c() + "°C");
+        temp2Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(1).getTemp_c() + "°C");
+        temp3Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(2).getTemp_c() + "°C");
+        temp4Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(3).getTemp_c() + "°C");
+        temp5Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(4).getTemp_c() + "°C");
+        temp6Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(5).getTemp_c() + "°C");
+        temp7Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(6).getTemp_c() + "°C");
+        temp8Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(7).getTemp_c() + "°C");
+        temp9Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(8).getTemp_c() + "°C");
+        temp10Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(9).getTemp_c() + "°C");
+        temp11Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(10).getTemp_c() + "°C");
+        temp12Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(11).getTemp_c() + "°C");
+        temp13Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(12).getTemp_c() + "°C");
+        temp14Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(13).getTemp_c() + "°C");
+        temp15Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(14).getTemp_c() + "°C");
+        temp16Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(15).getTemp_c() + "°C");
+        temp17Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(16).getTemp_c() + "°C");
+        temp18Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(17).getTemp_c() + "°C");
+        temp19Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(18).getTemp_c() + "°C");
+        temp20Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(19).getTemp_c() + "°C");
+        temp21Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(20).getTemp_c() + "°C");
+        temp22Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(21).getTemp_c() + "°C");
+        temp23Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(22).getTemp_c() + "°C");
+        temp24Label1.setText("" + weather.getForecast().getForecastday().get(0).getHour().get(23).getTemp_c() + "°C");    }
 
 }
